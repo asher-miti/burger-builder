2dc0231eff850606e3f156d1828bf60ea9cf3935
Asher <asher_3@hotmail.co.uk>
2020-08-21 15:05:19 +0100

    added an order now button along with an order summary for the burger

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./Modal.module.css";
+
+function Modal(props) {
+  return <div className={classes.Modal}>{props.children}</div>;
+}
+
+export default Modal;
